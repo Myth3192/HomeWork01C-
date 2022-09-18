5248c43a227bf64da4810588f66e3a0f5d283ee3
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-18 21:46:28 +0300

    First task complete

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, please enter two numbers");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Number " + num1 + " is more then " + num2);
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("Number " + num2 + " is more then " + num1);
+}
+else{
+    Console.WriteLine("Numbers are equal");
+}
+    
