fda44fd8b45b3da0a56a93d5b9a89e6b92ab75a2
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-19 17:51:07 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, pls, enter number");
+int num = int.Parse(Console.ReadLine());
+if (num%2==0)
+{
+    Console.WriteLine("Number is even");
+}
+else{
+    Console.WriteLine("Number is odd");
+}
