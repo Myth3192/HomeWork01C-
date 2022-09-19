557271616f699f5c350f68b0af8856b3f5b0d8d8
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-19 20:04:25 +0300

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, pls, enter number");
+int ren = int.Parse(Console.ReadLine());
+int num = 2;
+while (num<ren+1)
+{
+   Console.Write(num + " ");
+    num = num + 2; 
+}
+Console.WriteLine(" ");
