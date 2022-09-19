5940040b2b462ec4fc69fba0985e9be447698834
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-19 17:34:29 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, pls enter 3 numbers");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int maxnum = 0;
+if (num2 < num3)
+{
+    maxnum = num3;
+}
+else if (num1 < num2)
+{
+    maxnum = num2;
+}
+else
+{
+    maxnum = num1;
+}
+Console.WriteLine(maxnum);
